1c95682f4277a85384337d727c592ca2d3129b79
denicabaravdic <deni@phntmlabs.com>
2023-05-01 19:45:44 -0700

    added async await

--- JS Projects/Jokes - FetchAPI/script.js
+++ JS Projects/Jokes - FetchAPI/script.js
@@ -4,19 +4,38 @@ const jokeBtn = document.getElementById('jokeBtn');
 jokeBtn.addEventListener('click', generateJoke)
 
 
-const config = {
-    headers: {
-        'Accept': 'application/json'
-    }
-};
-
 generateJoke()
 
-function generateJoke() {
-    fetch('https://icanhazdadjoke.com/', config)
-    .then((response) => response.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    });
+// Better practice to use ASYNC AWAIT rather than .then for promises
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    const res = await fetch('https://icanhazdadjoke.com/', config);
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
+    
 
 }
+
+
+
+// const config = {
+//     headers: {
+//         'Accept': 'application/json'
+//     }
+// };
+
+// generateJoke()
+
+// function generateJoke() {
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     });
+
+// }
