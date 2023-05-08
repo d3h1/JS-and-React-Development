bb8e6d3935dcffd0b367dbe37b518ed08eeea440
d3h1 <denicabaravdic@gmail.com>
2023-05-08 16:15:07 -0700

    added main update javascript

--- JS Projects/Drink Water/script.js
+++ JS Projects/Drink Water/script.js
@@ -1,8 +1,10 @@
 // Gonna work on filling the small cups with this
 const smallCups = document.querySelectorAll('.cup-small')
 const litres = document.getElementById('litres')
-const percentage = document.getElementById('.percentage')
+const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
+
+updateBigCups()
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
@@ -22,8 +24,29 @@ function highlightCups(idx) {
             cup.classList.remove('full')
         }
     })
+    updateBigCups()
 }
 
 function updateBigCups() {
-    
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    }
+    else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    }
+    else {
+        remained.style.visibility = 'visible'
+        litres.innerText = `${2 - (250 * fullCups / 1000)}`
+    }
 }
