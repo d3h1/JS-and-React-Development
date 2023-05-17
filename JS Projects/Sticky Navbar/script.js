f51f65557a066fc125290e905da8b63bfb03f3fa
denicabaravdic <denicabaravdic@gmail.com>
2023-05-16 18:49:23 -0700

    fixing js

--- JS Projects/Sticky Navbar/script.js
+++ JS Projects/Sticky Navbar/script.js
@@ -1,8 +1,6 @@
 const nav = document.querySelector('.nav');
 window.addEventListener('scroll', fixNav);
 
-const links = document.getElementById('links')
-
 function fixNav() {
     if(window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
