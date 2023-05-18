eb88171709ca3ecd16db03789e1fe8dd60484abc
d3h1 <denicabaravdic@gmail.com>
2023-05-18 16:03:16 -0700

    password is dynamicallt and random generated now

--- JS Projects/Working Applications/Password Generator/script.js
+++ JS Projects/Working Applications/Password Generator/script.js
@@ -1,21 +1,77 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const clipboardEl = document.getElementById('clipboard')
+const generateEl = document.getElementById('generate')
+
+// Allows us to call on random functions based on the length of the wnated password
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbols
+}
+
+clipboardEl.addEventListener('click', () => {
+
+})
+
+// Generate the checked values
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+
+    // Outputs the result of the generated password
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+
+// Generates the password
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]) // Generates values based on only true values -- item => Object.values allows that by looking at True which is the 0 value
+
+    // If nothing is checked, you get nothing in the output
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    // Return final password to above event listener
+    return finalPassword
+}
+
+
+// Gets random lower char using ASCII
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
-
+// Gets random upper char using ASCII
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
-
+// Gets random number using ASCII
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
-
+// Gets random symbols
 function getRandomSymbols() {
     const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
-console.log(getRandomLower())
-console.log(getRandomUpper())
-console.log(getRandomNumber())
-console.log(getRandomSymbols())
