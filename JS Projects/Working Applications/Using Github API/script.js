067f794931b7f3009e7fbed21d1758a006cea679
denicabaravdic <denicabaravdic@gmail.com>
2023-05-17 19:20:47 -0700

    using axios to get user data

--- JS Projects/Working Applications/Using Github API/script.js
+++ JS Projects/Working Applications/Using Github API/script.js
@@ -1,1 +1,15 @@
-axios.put()
+
+const APIURL = 'https://api.github.com/users/';
+
+getUSer('d3h1')
+
+async function getUSer(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+        console.log(data)
+    } 
+    catch(err) {
+        console.log(err)
+    }
+}
+
