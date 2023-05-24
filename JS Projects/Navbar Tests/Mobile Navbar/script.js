a56933545eca9c2f6ef0108cc72eafd943cb169e
d3h1 <denicabaravdic@gmail.com>
2023-05-23 20:35:38 -0700

    created a node list that allows for tab switching

--- JS Projects/Navbar Tests/Mobile Navbar/script.js
+++ JS Projects/Navbar Tests/Mobile Navbar/script.js
@@ -1,5 +1,6 @@
 // CLASSES
-
+const imgContents = document.querySelectorAll('.content')
+const listContents = document.querySelectorAll('nav ul li')
 
 // IDS
 
@@ -8,6 +9,21 @@
 
 
 // EVENTS
+listContents.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
 
+        item.classList.add('active')
+        imgContents[idx].classList.add('show')
+    })
+})
 
 // FUNCTIONS
+const hideAllContents = () => {
+    imgContents.forEach(content => content.classList.remove('show'))
+}
+
+const hideAllItems = () => {
+    listContents.forEach(content => content.classList.remove('active'))
+}
