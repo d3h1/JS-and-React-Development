3628aab6814c0e0b8419fea32690ad9eb2798c77
d3h1 <denicabaravdic@gmail.com>
2023-05-23 18:42:17 -0700

    added manual image switching

--- JS Projects/Functionality/Image Carasoule/script.js
+++ JS Projects/Functionality/Image Carasoule/script.js
@@ -3,15 +3,25 @@ const img = document.querySelectorAll('#imgs img')
 
 // IDS
 const imgs = document.getElementById('imgs');
-const leftBtn = document.getElementById('prev')
-const rightBtn = document.getElementById()
+const leftBtn = document.getElementById('previous')
+const rightBtn = document.getElementById('next')
 
 // OTHER
 let idx = 0;
 let interval = setInterval(run, 2000)
 
 // EVENTS
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
 
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
 
 // FUNCTIONS
 function run() {
@@ -20,6 +30,20 @@ function run() {
     changeImage()
 }
 
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
 function changeImage() {
-    
+    // Checking if the image is at the end
+    if(idx > img.length - 1) {
+        idx = 0
+    }
+    // Checking if image is at beginning 
+    else if (idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
 }
