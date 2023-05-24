a2d8c04182bc1cb91dc4ccbf86ace40977964f84
d3h1 <denicabaravdic@gmail.com>
2023-05-23 20:11:47 -0700

    fetching from the API properly now

--- JS Projects/Working Applications/Pokedex/script.js
+++ JS Projects/Working Applications/Pokedex/script.js
@@ -2,20 +2,20 @@
 
 
 // IDS
-const pokecontainer = document.getElementById('poke-container')
+const pokeContainer = document.getElementById('poke-container')
 
 
 // OTHER
 const pokemonCount = 150
 const colors = {
     fire: '#F38466',
-    grass: '#DEFD30',
+    grass: '#98d7a5',
     electric: '#9FEB71',
     water: '#71DCEB',
     ground: '#f4e7da',
     rock: '#d5d5d4',
     fairy: '#fceaff',
-    poison: '#98d7a5',
+    poison: '#DEFD30',
     bug: '#f8d5a3',
     dragon: '#97b3e6',
     psychic: '#eaeda1',
@@ -23,6 +23,7 @@ const colors = {
     fighting: '#E6E0D4',
     normal: '#F5F5F5',
 }
+const mainTypes = Object.keys(colors)
 
 // EVENTS
 
@@ -38,7 +39,36 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+    const pokeTypes = pokemon.types.map(type => type.type.name)
+    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1)
+    const typeProper = type[0].toUpperCase() + type.slice(1)
+    const color = colors[type]
+    
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonCards = 
+    `
+    <div class="img-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: ${typeProper}</small>
+    </div>
+    `
+
+    pokemonEl.innerHTML = pokemonCards
+    pokeContainer.appendChild(pokemonEl)
 }
 
 fetchPokemons()
