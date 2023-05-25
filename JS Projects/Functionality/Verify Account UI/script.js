607ffec8fda0f4c16b150565680a3f7306f90c02
d3h1 <denicabaravdic@gmail.com>
2023-05-25 11:04:26 -0700

    got the numbers working and inputting

--- JS Projects/Functionality/Verify Account UI/script.js
+++ JS Projects/Functionality/Verify Account UI/script.js
@@ -1,11 +1,27 @@
 // CLASSES
-
+const codes = document.querySelectorAll('.code')
 
 // IDS
 
 
 // OTHER
+// !Automatically puts the cursor in the first number
+codes[0].focus()
 
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <= 9) {
+            setTimeout(() => {
+                codes[idx + 1].focus()
+            }, 1)
+        }
+        else if(e.key === 'Backspace') {
+            setTimeout(() => {
+                codes[idx - 1].focus()
+            }, 1)
+        }
+    })
+})
 
 // EVENTS
 
