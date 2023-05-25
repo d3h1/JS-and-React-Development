233f521722706d2265c4975859ee72b330441aef
d3h1 <denicabaravdic@gmail.com>
2023-05-24 20:33:07 -0700

    created blur effect

--- JS Projects/Functionality/Password Strength/script.js
+++ JS Projects/Functionality/Password Strength/script.js
@@ -2,10 +2,16 @@
 
 
 // IDS
-
+const password = document.getElementById('password')
+const background = document.getElementById('background')
 
 // OTHER
-
+password.addEventListener('input', (e) => {
+    const input = e.target.value
+    const length = input.length
+    const blurvalue = 20 - length * 2
+    background.style.filter = `blur(${blurvalue}px)`
+})
 
 // EVENTS
 
