ce321345a19333f679b58c625526d410efc4c7bf
denicabaravdic <denicabaravdic@gmail.com>
2023-05-27 22:13:23 -0700

    working on getting user data sourced

--- JS Projects/Functionality/Live User Filter/script.js
+++ JS Projects/Functionality/Live User Filter/script.js
@@ -2,12 +2,35 @@
 
 
 // IDS
-
+const results = document.getElementById('result')
+const filter = document.getElementById('filter')
 
 // OTHER
-
+const listItems = []
 
 // EVENTS
-
+getData()
 
 // FUNCTIONS
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=50')
+    const { results } = await res.json()
+
+    // Clear Results
+    results.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = 
+        `
+            <img src = "${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+    })
+}
