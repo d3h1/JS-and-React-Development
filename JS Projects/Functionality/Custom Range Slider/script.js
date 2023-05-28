465f3453ce7f676793d38a19866d4bf0255766ba
denicabaravdic <denicabaravdic@gmail.com>
2023-05-27 22:38:44 -0700

    we have a static label that displays correct num

--- JS Projects/Functionality/Custom Range Slider/script.js
+++ JS Projects/Functionality/Custom Range Slider/script.js
@@ -2,12 +2,20 @@
 
 
 // IDS
-
+const range = document.getElementById('range')
 
 // OTHER
 
 
 // EVENTS
+range.addEventListener('input', (e) => {
+    // !THE + converts this to a num
+    const value = +e.target.value
+    const label = e.target.nextElementSibling
 
+    
+
+    label.innerHTML = value
+})
 
 // FUNCTIONS
