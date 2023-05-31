fb1c052df28b529ba1de35e4e37440ef9d958908
denicabaravdic <denicabaravdic@gmail.com>
2023-05-30 20:34:33 -0700

    capitalizing the string

---
+++ JS Tools/Challenges/Capitalize/script.js
@@ -0,0 +1,6 @@
+// string is 'hello'
+const myString = 'hello'
+// making new string 'hello' and then uppercasing the first letter and then adding on the 'ello' on the end of it 
+const myNewString = myString.charAt(0).toUpperCase() + myString.slice(1)
+// print the new string
+console.log(myNewString)
