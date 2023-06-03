2e82411fd7b54724541e4623a82188a8a70b05e0
denicabaravdic <denicabaravdic@gmail.com>
2023-06-02 19:05:02 -0700

    rotuing and nest-routing

---
+++ NextJS Projects/my_courses/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis necessitatibus quasi sint debitis sit illum rem magni culpa ab modi porro non fugit mollitia fuga minima vel dolore iure aut expedita quibusdam, in perspiciatis amet! At exercitationem quibusdam eveniet et cupiditate accusamus voluptatem, debitis corrupti reprehenderit cumque. Architecto laudantium exercitationem aperiam quam nemo sunt excepturi fuga et dolor odio!</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ NextJS Projects/my_courses/app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Our Team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ut sint, reprehenderit aperiam in at pariatur aspernatur incidunt dicta, quis dolor facere quae optio necessitatibus nemo autem rem maiores. Dolorem?</p>
+    </div>
+  )
+}
+
+export default TeamPage
