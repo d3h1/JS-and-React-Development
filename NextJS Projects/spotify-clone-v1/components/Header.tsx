2bf901627bed036f3fdfd9ed0965926b06b7203e
d3h1 <denicabaravdic@gmail.com>
2023-06-06 14:30:27 -0700

    added more components, images. etc

--- NextJS Projects/spotify-clone-v1/components/Header.tsx
+++ NextJS Projects/spotify-clone-v1/components/Header.tsx
@@ -1,28 +1,80 @@
-'use client';
+"use client";
 
+// Next and React Imports
 import { useRouter } from "next/navigation";
-import { twMerge } from 'tailwind-merge'
+import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+
+// Icons
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 interface HeaderProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-    children,
-    className
-}) => {
-    const router = useRouter();
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
 
-    const handleLogout = () => {
-        // Handle Logout in the future
-    }
+  const handleLogout = () => {
+    // Handle Logout in the future
+  };
 
-    return ( 
-    <div className="{twMerge}">
-        
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+
+        {/* This is what happens in mobile view. Nothing else shows from before */}
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={26} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={26} />
+          </button>
+        </div>
+
+        {/* Signup and Signin */}
+        <div className="flex justify-between items-center gap-x-4">
+          {/* Doing this allows for dynamic features to work down the line  */}
+          <>
+            <div>
+                <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium"> 
+                    Sign Up
+                </Button>
+            </div>
+            <div>
+                <Button onClick={() => {}} className="bg-white px-6 py-2"> 
+                    Log In
+                </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
     </div>
-     );
-}
- 
+  );
+};
+
 export default Header;
