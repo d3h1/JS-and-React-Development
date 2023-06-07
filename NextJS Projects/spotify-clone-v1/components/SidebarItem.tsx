2159dc77a5a5326a98db4980882cde8235e4bcba
d3h1 <denicabaravdic@gmail.com>
2023-06-07 16:27:59 -0700

    commiting

--- NextJS Projects/spotify-clone-v1/components/SidebarItem.tsx
+++ NextJS Projects/spotify-clone-v1/components/SidebarItem.tsx
@@ -17,7 +17,7 @@ const SidebarItem:React.FC<SidebarItemProps> = ({
 }) => {
     return ( 
         // Active Prop when we are not in search activates the first link
-        <Link href={href} className={twMerge(`flex flex-row h-auto items-cneter w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`, active && 'text-white')}>
+        <Link href={href} className={twMerge(`flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`, active && 'text-white')}>
             <Icon size={26}/>
             <p className="truncate w-full">{label}</p>
         </Link>
