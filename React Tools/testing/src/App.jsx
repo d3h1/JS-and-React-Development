825357a714704e1715ff4ec5108028913c158f6f
denicabaravdic <denicabaravdic@gmail.com>
2023-06-08 21:25:42 -0700

    working with conditionals

--- React Tools/testing/src/App.jsx
+++ React Tools/testing/src/App.jsx
@@ -8,20 +8,31 @@ const App = () => {
         {id: 4, text: 'Comment 4'},
     ]
 
+    const loading = false;
+    const showComments = true;
+    const commentsLoop = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment,idx) => (
+                    <li key={idx}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    ) 
 
+    if(loading) return <h1>Loading ...</h1>
+
+    
     return (
         <div className="container">
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
 
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment,idx) => (
-                        <li key={idx}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
+            {/* If show comments is true, then show below */}
+            {showComments && commentsLoop}
+
+            
         </div>
 
   )
