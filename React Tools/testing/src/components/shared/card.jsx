b0e3cc11d10b5d4cd40c590749cfd0bd647df79b
denicabaravdic <denicabaravdic@gmail.com>
2023-06-19 12:32:10 -0700

    doing conditional styles

--- React Tools/testing/src/components/shared/card.jsx
+++ React Tools/testing/src/components/shared/card.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 function Card({ children, reverse }) {
+
+  // !Conditional Class - The move
   return (
-    <div className={`card ${reverse && 'reverse'}`}>
+    <div className={`card ${reverse && 'reverse'}`} >
       {children}
     </div>
   )
+
+  // ?Conditional Style -- Not the move
+  // return (
+  //   <div className='card' style={{
+  //     backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+  //     color: reverse ? '#fff' : '#000',
+  //   }}>
+  //     {children}
+  //   </div>
+  // )
 }
 
 export default Card
