c510551501ec7961d8fa1ae6ec79ea4f44dbe443
d3h1 <denicabaravdic@gmail.com>
2023-06-23 10:48:59 -0700

    formatting and structure

---
+++ React Projects/pdas/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from "./ProfileCard";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Our Digital Assistants</h1>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+};
+
+export default App;
