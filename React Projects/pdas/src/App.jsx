b754efff714476522cbd12b2ab250d29b2be135e
d3h1 <denicabaravdic@gmail.com>
2023-06-23 17:20:12 -0700

    finished cards and more

--- React Projects/pdas/src/App.jsx
+++ React Projects/pdas/src/App.jsx
@@ -1,16 +1,37 @@
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png'
-import SiriImage from './images/siri.png'
-import CortanaImage from './images/cortana.png'
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+import CortanaImage from "./images/cortana.png";
 
+import "bulma/css/bulma.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Our Digital Assistants</h1>
-      <ProfileCard image={SiriImage} name='Siri' handle='@siri90'/>
-      <ProfileCard image={AlexaImage} name='Alexa' handle='@alexa101' />
-      <ProfileCard image={CortanaImage} name='Cortana' handle='@cortana22' />
+      <div className="container">
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Our Digital Assistants</p>
+          </div>
+        </section>
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard image={SiriImage} name="Siri" handle="@siri90" />
+            </div>
+            <div className="column is-3">
+              <ProfileCard image={AlexaImage} name="Alexa" handle="@alexa101" />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                image={CortanaImage}
+                name="Cortana"
+                handle="@cortana22"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
--- React Projects/pdas/src/ProfileCard.jsx
+++ React Projects/pdas/src/ProfileCard.jsx
@@ -10,11 +10,21 @@ const ProfileCard = ({ name, image, handle }) => {
 
   // !!!! ABOVE IS THE BEST form of destructuring
 
-  return <div>
-    <img src={image} alt="prof-pic" />
-    <h3>{name}</h3>
-    <small>{handle}</small>
-  </div>;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="prof-pic" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{name}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
