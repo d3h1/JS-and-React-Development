4cfa6dac0486f5d1b23520954857bf3022b3b6f0
d3h1 <denicabaravdic@gmail.com>
2023-06-26 14:04:10 -0700

    starting new project

---
+++ React Projects/animals/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AnimalShow from './AnimalShow'
+
+const App = () => {
+  return (
+    <div>
+      Show List Here
+      
+    </div>
+  )
+}
+
+export default App
