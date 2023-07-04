edc300996384ae8647cddb767e7cf11bfafb4669
denicabaravdic <denicabaravdic@gmail.com>
2023-07-04 13:15:29 -0700

    song adding is coming along

--- NextJS Projects/spotify-clone-v1/components/UploadModal.tsx
+++ NextJS Projects/spotify-clone-v1/components/UploadModal.tsx
@@ -1,27 +1,52 @@
-"use client"
+"use client";
 
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState();
   const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
   const onChange = (open: boolean) => {
     if (!open) {
-      // Reset the form
-      uploadModal.onClose()
+      reset();
+      uploadModal.onClose();
     }
-  }
+  };
 
-  return ( 
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // upload to supabase
+  };
+
+  return (
     <Modal
-      title="Upload modal title"
-      description="Upload modal description"
+      title="Add a Song"
+      description="Upload an mp3 file"
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Upload content
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song Title"
+        />
+      </form>
     </Modal>
-   );
-}
- 
+  );
+};
+
 export default UploadModal;
