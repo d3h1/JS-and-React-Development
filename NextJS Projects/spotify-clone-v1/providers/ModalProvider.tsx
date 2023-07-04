7c69cd1da269a69b4a7d6d02974a4c439af96c04
denicabaravdic <denicabaravdic@gmail.com>
2023-07-03 18:09:47 -0700

    getting supabase connected and working with the application

--- NextJS Projects/spotify-clone-v1/providers/ModalProvider.tsx
+++ NextJS Projects/spotify-clone-v1/providers/ModalProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Modal from "@/components/Modal";
+import AuthModal from "@/components/AuthModal";
 
 import { useEffect, useState } from "react";
 
@@ -19,14 +19,7 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal
-        title="Test Model"
-        description="Test Description"
-        isOpen
-        onChange={() => {}}
-      >
-        Test Children{" "}
-      </Modal>
+      <AuthModal />
     </>
   );
 };
