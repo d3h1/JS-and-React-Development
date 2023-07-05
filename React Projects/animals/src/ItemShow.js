436d620438aaf0222c7905888728cc0898c5f155
d3h1 <denicabaravdic@gmail.com>
2023-07-05 14:27:13 -0700

    implementing items and doing a handle click function

--- React Projects/animals/src/ItemShow.js
+++ React Projects/animals/src/ItemShow.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
+
+import bird from './svg/cat.svg'
+import cat from './svg/cat.svg'
+import cow from './svg/cow.svg'
+import dog from './svg/dog.svg'
+import gator from './svg/gator.svg'
+import heart from './svg/heart.svg'
+import horse from './svg/horse.svg'
+
+
+
+const svgMap = {
+  bird, cat, cow, dog, gator, heart, horse
+}
 
 const ItemShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+  setClicks(clicks + 1)
+  }
   return (
-    <div>
-      <p>
-      {type}
-      
-      </p>
+    <div onClick={handleClick}>
+      <img alt='item' src={svgMap[type]} />
     </div>
   )
 }
