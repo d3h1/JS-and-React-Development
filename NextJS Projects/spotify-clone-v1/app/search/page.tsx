c3a2b03f1912b2b0d10ed325987b30b7d52793a7
denicabaravdic <denicabaravdic@gmail.com>
2023-07-06 18:11:47 -0700

    url is queried properly now

--- NextJS Projects/spotify-clone-v1/app/search/page.tsx
+++ NextJS Projects/spotify-clone-v1/app/search/page.tsx
@@ -1,4 +1,6 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
 
 interface SearchProps {
   searchParams: {
@@ -11,9 +13,17 @@ const Search = async ({ searchParams }: SearchProps) => {
 
   return <div
     className="
-      
+      bg-neutral-900 rounded-lg h-full w-full 
+      overflow-hidden overflow-y-auto
     "
-  >Search!</div>;
+  >
+    <Header className="from-bg-neutral-900"> 
+      <div className="mb-2 flex flex-col gap-y-6">
+        <h1 className="text-white text-3xl font-semibold">Search</h1>
+        <SearchInput />
+      </div>
+    </Header>
+  </div>;
 };
 
 export default Search;
