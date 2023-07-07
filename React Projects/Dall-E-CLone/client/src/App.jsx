e4c17242ab2576c6772753cc4e05ae1f15888636
d3h1 <denicabaravdic@gmail.com>
2023-07-07 10:06:02 -0700

    npm installing and starting app.js dev

--- React Projects/Dall-E-CLone/client/src/App.jsx
+++ React Projects/Dall-E-CLone/client/src/App.jsx
@@ -1,11 +1,20 @@
-function App() {
+import { BrowserRouter, Link, Routes } from 'react-router-dom'
 
+import { logo } from './assets/logo.svg'
+import { Home, CreatePost } from './pages'
+
+const App = () => {
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    
+    <BrowserRouter>
+      <header className='
+        w-full flex justify-between items-center
+       bg-white sm:px-8 px-4 py-4 border-b 
+       border-b-[#e6ebf4]
+       '>
+
+      </header>
+    </BrowserRouter>
   )
 }
 
---
+++ React Projects/Dall-E-CLone/client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+// This allows us to upload these in one single line in our App.jsx
+export {
+  Home,
+  CreatePost
+}
