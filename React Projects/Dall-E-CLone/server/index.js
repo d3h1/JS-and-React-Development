e932f10e244bdbd9dcbe17ce0875428214e1324f
denicabaravdic <denicabaravdic@gmail.com>
2023-07-07 17:34:41 -0700

    created routes and open connections to OpenAI API

--- React Projects/Dall-E-CLone/server/index.js
+++ React Projects/Dall-E-CLone/server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 // Allows us to pull env variables from the dotenv file
 dotenv.config();
@@ -11,6 +13,10 @@ const app = express();
 // Adding additional middlewares
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// Creating API endpoints that hook onto from front end 
+app.use('/api/v1/posts', postRoutes)
+app.use('/api/v1/posts', dalleRoutes)
 
 // Allows us to get responses and requests
 app.get('/', async (req,res) => {
